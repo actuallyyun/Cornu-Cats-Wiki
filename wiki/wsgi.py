2276ce7c466729ed8fb6869053cd3ecafa01460b
Yun <irene.jiyun@gmail.com>
2021-12-09 14:01:10 +0100

    modified enviroment default

--- wiki/wsgi.py
+++ wiki/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO__MODULE', 'wiki.')
+os.environ.setdefault('DJANGO__MODULE', 'wiki.settings')
 
 application = get_wsgi_application()
